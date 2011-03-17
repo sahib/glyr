039a660413494c7ba3ae39263bd51aa95d4244d8
Chris P <sahib@online.de>
2011-03-17 22:25:51 +0100

    Forgot google.c

---
+++ lib/photos/google.h
@@ -0,0 +1,28 @@
+/***********************************************************
+* This file is part of glyr
+* + a commnadline tool and library to download various sort of musicrelated metadata.
+* + Copyright (C) [2011]  [Christopher Pahl]
+* + Hosted at: https://github.com/sahib/glyr
+*
+* glyr is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* glyr is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with glyr. If not, see <http://www.gnu.org/licenses/>.
+**************************************************************/
+
+#ifndef P_GOOGLE_H
+#define P_GOOGLE_H
+
+#include "../core.h"
+const char   * photos_google_url(GlyQuery * sets);
+GlyCacheList * photos_google_parse(cb_object *capo);
+
+#endif
