477ce7da37d09dabdd6c60a4ce4de9a6a24d6f87
Chris P <sahib@online.de>
2012-02-04 11:52:51 +0100

    Added ,,default'' config.h

---
+++ lib/config.h
@@ -0,0 +1,43 @@
+/**
+ * SECTION:config
+ * @short_description: Compiletime Information
+ * @title: Config 
+ * @section_id:
+ * @stability: Stable
+ * @include: glyr/config.h
+ *
+ * Misc. version checking macros and other defines
+ * that may vary between builds.
+ */
+
+#define GLYR_VERSION_MAJOR "0"
+#define GLYR_VERSION_MINOR "9"
+#define GLYR_VERSION_MICRO "4"
+
+#define GLYR_VERSION_MAJOR_INT 0
+#define GLYR_VERSION_MINOR_INT 9
+#define GLYR_VERSION_MICRO_INT 4
+
+/**
+ * GLYR_CHECK_VERSION:
+ * @X: Macro version.
+ * @Y: Mino version.
+ * @Z: Micro version,
+ *
+ * Version to check glyr's version.
+ * Example:
+ * <informalexample>
+ * <programlisting>
+ * #if GLYR_CHECK_VERSION(0,8,7)
+ *      puts("Version is at least 0.87!");
+ * #endif
+ * </programlisting>
+ * </informalexample>
+*/
+#define GLYR_CHECK_VERSION(X,Y,Z) (X <= GLYR_VERSION_MAJOR_INT && Y <= GLYR_VERSION_MINOR_INT && Z <= GLYR_VERSION_MICRO_INT)
+
+#define GLYR_VERSION_NAME  "Catholic Cat"
+#define GLYR_DEBUG         TRUE
+
+/* Message output */
+#define GLYR_OUTPUT stderr
