0167b2604e385222ebaa9babcb635dbb8db06c75
Chris P <sahib@online.de>
2012-03-10 15:35:12 +0100

    Fixed crash in relations:musicbrainz (deref of a NULL pointer)

--- lib/intern/common/musicbrainz.c
+++ lib/intern/common/musicbrainz.c
@@ -1,22 +1,22 @@
 /***********************************************************
-* This file is part of glyr
-* + a commnadline tool and library to download various sort of musicrelated metadata.
-* + Copyright (C) [2011]  [Christopher Pahl]
-* + Hosted at: https://github.com/sahib/glyr
-*
-* glyr is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* glyr is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with glyr. If not, see <http://www.gnu.org/licenses/>.
-**************************************************************/
+ * This file is part of glyr
+ * + a commnadline tool and library to download various sort of musicrelated metadata.
+ * + Copyright (C) [2011]  [Christopher Pahl]
+ * + Hosted at: https://github.com/sahib/glyr
+ *
+ * glyr is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * glyr is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with glyr. If not, see <http://www.gnu.org/licenses/>.
+ **************************************************************/
 
 #include "musicbrainz.h"
 #include "../../stringlib.h"
@@ -56,14 +56,14 @@ const gchar * generic_musicbrainz_url(GlyrQuery * sets)
 
     switch(please_what_type(sets))
     {
-    case GLYR_TYPE_TAG_TITLE :
-        return g_strdup_printf("http://musicbrainz.org/ws/1/track/?type=xml&title=%s&artist=%s&release=%s",wrap_t,wrap_a,wrap_b);
-    case GLYR_TYPE_TAG_ALBUM :
-        return g_strdup_printf("http://musicbrainz.org/ws/1/release/?type=xml&title=%s&artist=%s",wrap_b,wrap_a);
-    case GLYR_TYPE_TAG_ARTIST:
-        return g_strdup_printf("http://musicbrainz.org/ws/1/artist/?type=xml&name=%s",wrap_a);
-    default:
-        return NULL;
+        case GLYR_TYPE_TAG_TITLE :
+            return g_strdup_printf("http://musicbrainz.org/ws/1/track/?type=xml&title=%s&artist=%s&release=%s",wrap_t,wrap_a,wrap_b);
+        case GLYR_TYPE_TAG_ALBUM :
+            return g_strdup_printf("http://musicbrainz.org/ws/1/release/?type=xml&title=%s&artist=%s",wrap_b,wrap_a);
+        case GLYR_TYPE_TAG_ARTIST:
+            return g_strdup_printf("http://musicbrainz.org/ws/1/artist/?type=xml&name=%s",wrap_a);
+        default:
+            return NULL;
     }
 }
 
@@ -81,23 +81,23 @@ const gchar * get_mbid_from_xml(GlyrQuery * s, GlyrMemCache * c, gint * offset)
 
     switch(please_what_type(s))
     {
-    case GLYR_TYPE_TAG_TITLE:
-        checkstring = "<title>";
-        searchterm  = "<track ";
-        comparestr  = s->title;
-        break;
-    case GLYR_TYPE_TAG_ALBUM:
-        checkstring = "<title>";
-        searchterm  = "<release ";
-        comparestr  = s->album;
-        break;
-    case GLYR_TYPE_TAG_ARTIST:
-        checkstring = "<name>";
-        searchterm  = "<artist ";
-        comparestr  = s->artist;
-        break;
-    default:
-        glyr_message(1,s,"Warning: (tags/musicbrainz.c) Unable to determine type.\n");
+        case GLYR_TYPE_TAG_TITLE:
+            checkstring = "<title>";
+            searchterm  = "<track ";
+            comparestr  = s->title;
+            break;
+        case GLYR_TYPE_TAG_ALBUM:
+            checkstring = "<title>";
+            searchterm  = "<release ";
+            comparestr  = s->album;
+            break;
+        case GLYR_TYPE_TAG_ARTIST:
+            checkstring = "<name>";
+            searchterm  = "<artist ";
+            comparestr  = s->artist;
+            break;
+        default:
+            glyr_message(1,s,"Warning: (tags/musicbrainz.c) Unable to determine type.\n");
     }
 
     const gchar * mbid = NULL;
@@ -108,7 +108,7 @@ const gchar * get_mbid_from_xml(GlyrQuery * s, GlyrMemCache * c, gint * offset)
         gsize nlen = (sizeof ID_BEGIN) - 1;
         gsize clen = strlen(checkstring);
 
-        while( (node = strstr(node,searchterm)) && mbid == NULL)
+        while(node && (node = strstr(node,searchterm)) && mbid == NULL)
         {
             if(!(node = strstr(node,ID_BEGIN)))
                 continue;
@@ -116,19 +116,19 @@ const gchar * get_mbid_from_xml(GlyrQuery * s, GlyrMemCache * c, gint * offset)
             if(!(search_check = strstr(node,checkstring)))
                 continue;
 
-	    search_check += clen;
-	    gchar * to_compare = copy_value(search_check,strstr(search_check,"</"));
-	    if(to_compare != NULL)
-	    {
-		    if(levenshtein_strnormcmp(s,to_compare,comparestr) <= s->fuzzyness)
-		    {
-			    mbid = (gchar*)copy_value(node+nlen,strchr(node+nlen,'"'));
-		    }
-		    g_free(to_compare);
-	    }
-	    node += (sizeof ID_BEGIN) - 1;
-	}
-	*offset = node - c->data;
+            search_check += clen;
+            gchar * to_compare = copy_value(search_check,strstr(search_check,"</"));
+            if(to_compare != NULL)
+            {
+                if(levenshtein_strnormcmp(s,to_compare,comparestr) <= s->fuzzyness)
+                {
+                    mbid = (gchar*)copy_value(node+nlen,strchr(node+nlen,'"'));
+                }
+                g_free(to_compare);
+            }
+            node += (sizeof ID_BEGIN) - 1;
+        }
+        *offset = node - c->data;
     }
     return mbid;
 }
@@ -138,36 +138,36 @@ const gchar * get_mbid_from_xml(GlyrQuery * s, GlyrMemCache * c, gint * offset)
 /* Returns only a parseable memcache */
 GlyrMemCache * generic_musicbrainz_parse(cb_object * capo, gint * last_mbid, const gchar * include)
 {
-	gsize offset = 0;
-	const gchar * mbid = NULL;
-	GlyrMemCache * info = NULL;
+    gsize offset = 0;
+    const gchar * mbid = NULL;
+    GlyrMemCache * info = NULL;
 
-	while(offset < capo->cache->size && info==NULL && (mbid = get_mbid_from_xml(capo->s,capo->cache,last_mbid)))
-	{
-		if(mbid != NULL)
-		{
-			const gchar * type = NULL;
-			switch(please_what_type(capo->s))
-			{
-				case GLYR_TYPE_TAG_TITLE:
-					type = "track";
-					break;
-				case GLYR_TYPE_TAG_ALBUM:
-					type = "release";
-					break;
-				case GLYR_TYPE_TAG_ARTIST:
-					type = "artist";
-					break;
-			}
+    while(offset < capo->cache->size && info==NULL && (mbid = get_mbid_from_xml(capo->s,capo->cache,last_mbid)))
+    {
+        if(mbid != NULL)
+        {
+            const gchar * type = NULL;
+            switch(please_what_type(capo->s))
+            {
+                case GLYR_TYPE_TAG_TITLE:
+                    type = "track";
+                    break;
+                case GLYR_TYPE_TAG_ALBUM:
+                    type = "release";
+                    break;
+                case GLYR_TYPE_TAG_ARTIST:
+                    type = "artist";
+                    break;
+            }
 
-			gchar * info_page_url = g_strdup_printf("http://musicbrainz.org/ws/1/%s/%s?type=xml&inc=%s",type,mbid,include);
-			if(info_page_url)
-			{
-				info = download_single(info_page_url,capo->s,NULL);
-				g_free(info_page_url);
-			}
-			g_free((gchar*)mbid);
-		}
-	}
-	return info;
+            gchar * info_page_url = g_strdup_printf("http://musicbrainz.org/ws/1/%s/%s?type=xml&inc=%s",type,mbid,include);
+            if(info_page_url)
+            {
+                info = download_single(info_page_url,capo->s,NULL);
+                g_free(info_page_url);
+            }
+            g_free((gchar*)mbid);
+        }
+    }
+    return info;
 }
