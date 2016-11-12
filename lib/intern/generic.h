8db01d6f8d26c71df25f487a4fb814389f7b4026
Chris P <sahib@online.de>
2016-11-12 15:42:36 +0100

    License: change to LGPLv3 from GPLv3 (see #82)

--- lib/intern/generic.h
+++ lib/intern/generic.h
@@ -1,20 +1,20 @@
 /***********************************************************
  * This file is part of glyr
  * + a command-line tool and library to download various sort of musicrelated metadata.
- * + Copyright (C) [2011-2012]  [Christopher Pahl]
+ * + Copyright (C) [2011-2016]  [Christopher Pahl]
  * + Hosted at: https://github.com/sahib/glyr
  *
  * glyr is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * glyr is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Lesser General Public License
  * along with glyr. If not, see <http://www.gnu.org/licenses/>.
  **************************************************************/
 
